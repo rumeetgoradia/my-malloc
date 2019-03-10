efe9858ace8a2cf777bbc857dc3f783416fed6b7
Rumeet U. Goradia <rumeet.goradia@gmail.com>
2019-03-10 14:16:24 -0400

    Add comments to mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -2,57 +2,48 @@
 #include<stdlib.h>
 #include "mymalloc.h"
 
-/*BASE MAGIC NUMBER: 34927*/
+/*MAGIC NUMBER: 34927*/
 
+/* calc() --> returns the sum of two passed bytes, assumes second is more significant byte */
 size_t calc (unsigned char first, unsigned char second) {
 	return (second << 8) + first;	 
 }
-/*
-void update_magic (int index, size_t difference) {
-	size_t size = calc(myblock[index-2], [index-1]);
-	int i = index + calc_size;
-	for (i = index + calc_size; i < 4096; i += calc_size) { 
-		if (myblock[i + 5] == 'f' || myblock[i + 5] == 't') {
-			size_t magic = calc(myblock[i + 1], myblock[i + 2]);
-			magic += difference;
-			myblock[i + 1] = magic;
-			magic = magic >> 8;
-			myblock[i + 2] = magic;
-		} else {
-			return;
-		}
-	}
-} */
 
+/* split() --> create new metadata block after the current one
+ *	1. find the difference between available bytes and sum of metadata and allocated bytes
+ *	2. set new block to free metadata with remaining size if still space in array for metadata
+ */
 void split (int index, size_t size, size_t available) {
-//	size_t adjusted_magic = prev_magic + 5 + size;
 	size_t remaining_size = available - (3 + size);
-//	printf("SPLIT: %lu\n", remaining_size);
-	if (index + size < 4091) {
-//		update_magic(index, adjusted_magic - prev_magic, 1);
-//		myblock[index + size + 1] = (unsigned char) adjusted_magic;
-//		adjusted_magic = adjusted_magic >> 8;
-//		myblock[index + size + 2] = (unsigned char) adjusted_magic;
+	if (index + size < 4093) {
 		myblock[index + size + 1] = (unsigned char) remaining_size;
 		remaining_size = remaining_size >> 8;
 		myblock[index + size + 2] = (unsigned char) remaining_size;
 		myblock[index + size + 3] = 'f';
 	} 
 }
-
+/* coalesce() --> combine two free chunks
+ *	1. find sum of current pointer's size, next pointer's size, and size of metadata
+ *	2. set current pointer's size to this sum
+ */ 
 void coalesce (char * ptr, size_t curr_size, size_t next_size) {
 	size_t new_size = curr_size + next_size + 3;
 	*(ptr - 3) = (unsigned char) new_size;
 	new_size = new_size >> 8;
 	*(ptr - 2) = (unsigned char) new_size;
 }
-
+/* init() --> first malloc call, return first pointer
+ * 	1. ensure there is enough space for necessary metadata
+ * 	2. set magic number so that init isn't called again
+ * 	3. set current block's size and change to in-use
+ * 	4. call split to form new metadata block
+ * 	5. return pointer to first index after metadata
+ */
 void * init (size_t size, char * file, size_t line) {
-	if (size > 4093) {
+	if (size > 4091) {
 		fprintf(stderr, "Error in file \"%s\" at line #%lu.\nNot enough memory. Please try again with a smaller size.\n", file, (unsigned long int) line);
 		return NULL;
 	} 
-//	printf("SIZE: %lu\n", size);
 	myblock[0] = (unsigned char) 34927;
 	size_t magic2 = 34927 >> 8;
 	myblock[1] = (unsigned char) magic2;
@@ -60,11 +51,17 @@ void * init (size_t size, char * file, size_t line) {
 	size_t size2 = size >> 8;
 	myblock[3] = (unsigned char) size2;
 	myblock[4] = 't';
-//	printf("first two size bytes: %X %X\n", myblock[2], myblock[3]);
-	split(4, size, 4091); //pass in index of in_use
+	split(4, size, 4091);
 	return (void *) (myblock + 5);
 }
 
+/* create() --> return new pointer
+ *	1. set pointer to in-use
+ *	2. only proceed to change metadata if there is enough space to create new metadata
+ *	3. set size in metadata
+ *	4. call split to form metadata block
+ *	5. return pointer to first index after metadata
+ */ 
 void * create (int index, size_t size, size_t available) {
 	myblock[index - 1] = 't';
 	if (available - size >= 3) {
@@ -76,6 +73,14 @@ void * create (int index, size_t size, size_t available) {
 	return (void *) (myblock + index);	
 }
 
+/* mymalloc() --> dynamically allocate pointers using 3-byte metadata
+ *	1. ensure size is valid
+ *	2. check for magic number; if none, return init()'s return pointer
+ *	3. traverse through current list of pointers, incrementing by current pointer's size each time
+ *	4. if pointer is free and has enough space, return create()'s return pointer
+ *	5. check if possible to coalesce; if possible, call coalesce() and return create()'s return pointer
+ *	6. if reached end of loop, either no free space or just not enough space for this pointer, return NULL
+ */ 
 void * mymalloc (size_t size, char * file, size_t line) {
 	if (size <= 0) {
 		fprintf(stderr, "Error in file \"%s\" at line #%lu.\nInvalid size for dynamic allocation. Please try again.\n", file, (unsigned long int) line);
@@ -84,35 +89,37 @@ void * mymalloc (size_t size, char * file, size_t line) {
 		return init(size, file, line);
 	}
 	int i = 5;
-//	printf("first two size bytes: %X %X\n", myblock[2], myblock[3]);
 	size_t calc_size = calc((unsigned char) myblock[2], (unsigned char) myblock[3]);
 	size_t old_size = calc_size;
-//	printf("SIZE 0: %lu\n", calc_size);
-//	size_t magic_num = calc((unsigned char) myblock[0], (unsigned char) myblock[1]);
+	int any_free = 0;
 	for (i = 5; i < 4096; i += old_size + 3) {
 		old_size = calc_size;
-//		printf("flag: %c\n", myblock[i - 1]);
+		if (myblock[i - 1] == 'f' && calc_size != 0) {
+			any_free = 1;
+		}
 		if (calc_size >= size && myblock[i - 1] == 'f') {
 			return create(i, size, calc_size);
 		}
-//		printf("got past first if\n");
 		int next_size = calc((unsigned char) myblock[i + calc_size], (unsigned char) myblock[i + calc_size + 1]);
-//		printf("got past next_size\n");
 		if (myblock[i - 1] == 'f' && myblock[i + calc_size + 2] == 'f' && calc_size + next_size + 3 >= size) {
 			coalesce((char *)(myblock + i), calc_size, next_size);
 			return create(i, size, calc_size + next_size + 3);
 		}
-//		printf("got past potential coalesce\n");
-//		magic_num = calc((unsigned char) myblock[i + calc_size], (unsigned char) myblock[i + calc_size + 1];
 		calc_size = next_size;			
-//		printf("SIZE %d: %lu\n", i, calc_size);
 	}
-	fprintf(stderr, "Error in file \"%s\" at line #%lu.\nNot enough memory. Please try again with a smaller size.\n", file, (unsigned long int) line);
+	if (any_free == 1) {
+		fprintf(stderr, "Error in file \"%s\" at line #%lu.\nNot space large enough to contain this pointer. Please try again with a smaller size.\n", file, (unsigned long int) line);
+	} else {
+		fprintf(stderr, "Error in file \"%s\" at line #%lu.\nNo free space. Free some pointers to create space for this allocation.\n", file, (unsigned long int) line);
+	}
 	return NULL;
 }
-
+/* myfree() --> sets current pointer to not in use
+ *	1. run many tests to ensure passed-in pointer is valid
+ *	2. if all tests passed, set pointer's in-use byte to false in its metadata
+ *	3. check if possible to coalesce, and do so if yes
+ */ 
 void myfree (void * ptr, char * file, size_t line) {
-//	printf("ptr: %x\n", (char *) ptr);
 	if ((char* )ptr < (char *) myblock || (char *) ptr >= (char *) (myblock + 4096) || ((*((char *)(ptr - 1)) != 't' && *((char*)(ptr - 1)) != 'f'))) {
 		fprintf(stderr, "Error in file \"%s\" at line #%lu.\nInvalid use of free(). This pointer was not dynamically allocated through malloc().\n", file, (unsigned long int) line);
 		return;
@@ -129,6 +136,5 @@ void myfree (void * ptr, char * file, size_t line) {
 			size_t next_size = calc((unsigned char) *((char *)(ptr + size)), (unsigned char) *((char *)(ptr + size + 1)));
 			coalesce((char *)ptr, size, next_size);
 		}
-//		printf("setting add %x to NULL\n", (char *) ptr);
 	}
 }
